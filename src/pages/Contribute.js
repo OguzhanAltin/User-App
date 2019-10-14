8bc8ee460fda27dfd8d20b3a1ec88b2c5801acf9
OguzhanAltin <oguz_altin@hotmail.com>
2019-10-14 06:40:50 +0300

    first commit

---
+++ src/pages/Contribute.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function Contribute() {
+    return (
+        <div>
+            <h3>Project Files and Contribute to Project</h3>
+            <p>
+                You Can Download full project <a href = "https://github.com/OguzhanAltin">this</a> github page.
+            </p>
+        </div>
+    )
+}
+export default Contribute;
